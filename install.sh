e631b02fc0cc7ea4a3e52bfc27d973f39f7d0ff6
Noah Hunter <nhhntr11@gmail.com>
2017-10-13 20:11:04 +0000

    Minor changes

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-apt-get install build-essential libboost-all-dev libssl-dev
+apt-get install -y build-essential libboost-all-dev libssl-dev
 make
 cp ducatus-seeder.service /etc/systemd/system/
+systemctl daemon-reload
 systemctl start ducatus-seeder.service
 systemctl enable ducatus-seeder.service
